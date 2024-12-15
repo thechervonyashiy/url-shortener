fa0e839ef83e5ad07c9209d55c8e669208fae1cb
Ivan Chervonyashiy <thechervonyashiy@gmail.com>
2024-12-15 22:03:22 +0300

    1

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"url-shortener/internal/config"
+)
+
+func main() {
+	// TODO: init config: cleanenv
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: run server
+}
